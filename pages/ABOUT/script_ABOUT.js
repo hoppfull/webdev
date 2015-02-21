282f5e7da91cd5739f0a0f6f736f6f3b96c28b68
hoppfull <hoppfull@gmail.com>
2015-02-21 19:17:30 +0100

    overhaul
    whatevs

---
+++ pages/ABOUT/script_ABOUT.js
@@ -0,0 +1,12 @@
+var ABOUT = { //Here we store all properties and functions specifically used by about page
+	file: "./pages/ABOUT/content_ABOUT.html",
+	target: "content_ABOUT",
+	
+	onOpen: function(){
+		alert("ABOUT opened!");
+	},
+	
+	onClose: function(){
+		alert("ABOUT closed!");
+	}
+}
---
+++ pages/WEBGL/script_WEBGL.js
@@ -0,0 +1,12 @@
+var WEBGL = { //Here we store all properties and functions specifically used by webgl page
+	file: "./pages/WEBGL/content_WEBGL.html",
+	target: "content_WEBGL",
+	
+	onOpen: function(){
+		alert("WEBGL opened!");
+	},
+	
+	onClose: function(){
+		alert("WEBGL closed!");
+	}
+}
